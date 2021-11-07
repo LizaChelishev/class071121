384de356accec32a1e640830ae588817f965e820
LizaChelishev <lizachelishev@gmail.com>
2021-11-07 18:54:37 +0200

    done

---
+++ question1.py
@@ -0,0 +1,8 @@
+# example:
+# print( {i:i for i in range(10) if i > 0} )
+
+# 1
+# create the following dictionary:
+# { 1: 1, 2: 4, 3: 9, 4: 16 ... 10: 100}
+
+print( {i:i**2 for i in range(11) if i > 0} )
